7bb70a7f5cbb7ce7d5ecf763ab00ff6cc974023f
GoldWires <67865290+GoldWires@users.noreply.github.com>
2020-10-02 00:39:51 +0300

    fix: implemented functions for calculating arrays

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array) {
+    if (array.length === 0) return 0;
+    else{
+      var array_min = array[0];
+      for (var i = 0; i < array.length; i++) {
+        if (array[i] < array_min) {
+          array_min = array[i];
+        }
+      }
+      return array_min;
+    }
+  } else return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array) {
+    if (array.length === 0) return 0;
+    else{
+      var array_max = array[0];
+      for (var i = 0; i < array.length; i++) {
+        if (array[i] > array_max) {
+          array_max = array[i];
+        }
+      }
+      return array_max;
+    }
+  }
+  else return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array) {
+    if (array.length === 0) return 0;
+    else{
+      var array_avg = 0;
+      for (var i = 0; i < array.length; i++) 
+        array_avg = array_avg + array[i];
+        array_avg = array_avg/i;
+        return array_avg;
+    }
+  } else return 0;
 }
